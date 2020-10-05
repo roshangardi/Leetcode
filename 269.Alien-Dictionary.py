eeb376e32b8a4cb616e3b40105565f0f3c0ff9aa
Roshan <roshan.gardi002@gmail.com>
2020-10-05 18:53:28 +0530

    Added Alien Dictionary code

--- 269.Alien-Dictionary.py
+++ 269.Alien-Dictionary.py
@@ -1,31 +1,43 @@
-# Python3
+from collections import defaultdict, Counter, deque
 
-# Definition for a undirected graph node
-class UndirectedGraphNode:
-    def __init__(self, x):
-        self.label = x
-        self.neighbors = []
 
-class Solution:
-    def __init__(self):
-        self.node_map = {}
+def alienOrder(self, words: List[str]) -> str:
+    # Step 1 : Build graph of words
+    graph = defaultdict(set)
+    indegree = Counter({c: 0 for word in words for c in word})
 
-    # @param node, a undirected graph node
-    # @return a undirected graph node
-    def cloneGraph(self, node):
+    # compare adjacent words
+    for i in range(len(words) - 1):
+        first_word = words[i]
+        second_word = words[i + 1]
+        for j in range(min(len(first_word), len(second_word))):
+            # find the first mismatch alphabet
+            if first_word[j] != second_word[j]:
+                # either put the alphabet in the graph or add an edge to the graph
+                if second_word[j] not in graph[first_word[j]]:
+                    graph[first_word[j]].add(second_word[j])
+                    # change indegree
+                    indegree[second_word[j]] += 1
+                break
+            else:
+                if len(second_word) < len(first_word): return ""
+    # Step 2 : Topo Sort
+    output = []
+    # add elements with indegree == 0 to queue
+    queue = deque([c for c in indegree if indegree[c] == 0])
+    while queue:
+        c = queue.popleft()
+        # add to sorted order
+        output.append(c)
+        # change the indegrees of the end vertices and add to queue if new indegree == 0
+        for d in graph[c]:
+            indegree[d] -= 1
+            if indegree[d] == 0:
+                queue.append(d)
 
-        # approach: DFS with recursion
+    # this means there was a cycle because length of our final sort is not equal to the length of items we had in the
+    # indegree list
+    if len(output) < len(indegree):
+        return ""
 
-        if not node:
-            return None
-
-        if node.label in self.node_map:
-            return self.node_map[node.label]
-
-        copy_node = UndirectedGraphNode(node.label)
-        self.node_map[node.label] = copy_node
-
-        for neighbor in node.neighbors:
-            copy_node.neighbors.append(self.cloneGraph(neighbor))
-
-        return copy_node
+    return "".join(output)
