ad22eccbe21c46f4cbd0816314a35c43a8d411c8
Roshan <roshan.gardi002@gmail.com>
2020-10-30 23:01:55 +0530

    optimized: subtree of another tree

--- 572.Subtree_of_another_tree.py
+++ 572.Subtree_of_another_tree.py
@@ -1,4 +1,5 @@
 # Self written code. Not optimized code. Try to optimize the code further.
+# Optimized code below
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -40,6 +41,39 @@ class Solution:
 
         return False
 
+
+# Advanced  approach, O( | s | + | t |) (Merkle hashing): For  each node in a tree, we can create node.merkle,
+# a hash representing it 's subtree. This  hash is formed by hashing the concatenation of the merkle of the left
+# child, the node's value, and the merkle of the right child. Then, two trees are identical if and only if the merkle
+# hash of their roots are equal (except when there is a hash collision.) From there, finding the answer is
+# straightforward: we simply check if any node in s has node.merkle == t.merkle
+#
+# def isSubtree(self, s, t):
+#     from hashlib import sha256
+#     def hash_(x):
+#         S = sha256()
+#         S.update(x)
+#         return S.hexdigest()
+#
+#     def merkle(node):
+#         if not node:
+#             return '#'
+#         m_left = merkle(node.left)
+#         m_right = merkle(node.right)
+#         node.merkle = hash_(m_left + str(node.val) + m_right)
+#         return node.merkle
+#
+#     merkle(s)
+#     merkle(t)
+#
+#     def dfs(node):
+#         if not node:
+#             return False
+#         return (node.merkle == t.merkle or
+#                 dfs(node.left) or dfs(node.right))
+#
+#     return dfs(s)
+
 master = TreeNode(3)
 master.left = TreeNode(4)
 master.right = TreeNode(5)
@@ -54,3 +88,4 @@ subtree.right = TreeNode(2)
 
 sol = Solution()
 print(sol.isSubtree(master,subtree))
+
