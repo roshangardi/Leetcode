c09aa250acf19c28195a5d96b3dbaba05b0b39d7
Roshan <roshan.gardi002@gmail.com>
2020-11-18 19:26:05 +0530

    Optimized: Remove Nth node from end of LinkedList

--- 19. Remove Nth Node from End of LinkedList.py
+++ 19. Remove Nth Node from End of LinkedList.py
@@ -1,11 +1,12 @@
 # Created my own code to delete Nth node from the last of the LinkedList.
-
-
+# Note: Try to make use of dummy nodes to get rid of corner cases like empty list or single node list etc.
+# This is not a good solution considering first reversing and then traversing of the list and then again reversing.
+# So a better solution is a simple math trick to find correct node to delete, with slow and fast pointers. Check last code.
+""" My own Code:
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
 
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
@@ -48,6 +49,49 @@ class Solution:
 node = ListNode(1)
 node.next = ListNode(2)
 node.next.next = ListNode(3)
+
+sol = Solution()
+head = sol.removeNthFromEnd(node, 2)  # Here 2 is the nth node from last. (2nd node from last)
+
+while head:
+    print(head.val)
+    head = head.next
+"""
+
+
+# Optimized code:
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    """In this code we are keeping 'n' distance between both pointers and incrementing both at constant speed,
+    and when fast pointer reaches beyond last node/None value, it is similar to having
+    'n' distance from the end till the slow pointer and then we can delete node at slow pointer"""
+
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy_node = ListNode(-1)
+        dummy_node.next = head
+
+        slow = dummy_node  # Point this to dummy_node and not Head, else in case of only two nodes, it will give error.
+        fast = dummy_node
+
+        for i in range(n + 1):  # Using dummy node, hence extra 1 (i.e. n+1)
+            fast = fast.next  # keep distance 'n' between fast and slow pointer
+
+        while fast:  # Then increment both with equal speed
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next  # Remove the node by pointing to it's next.
+        return dummy_node.next  # Returning the actual head.
+
+
+node = ListNode(1)
+node.next = ListNode(2)
+# node.next.next = ListNode(3)
 
 sol = Solution()
 head = sol.removeNthFromEnd(node, 2)  # Here 2 is the nth node from last. (2nd node from last)
